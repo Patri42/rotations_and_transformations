95f6fbcc9b64ef97063db5021a8f1e0e35937ba0
Patri42 <patrick.borzych@gmail.com>
2024-01-09 14:41:19 +0100

    update

--- euler_angles_to_dcm.py
+++ euler_angles_to_dcm.py
@@ -28,6 +28,25 @@ def rotationEulerXYZ (phi, theta, psi):
     R = np.matmul(Rx, np.matmul(Ry, Rz))
     return R
 
+def rotationEulerZXZ (phi, theta, psi):
+    Rz2 = rotationZ(phi)
+    Rx = rotationX (theta)
+    Rz1 = rotationZ (psi)
+    R = np.matmul(Rz2, np.matmul(Rx, Rz1))
+    return R
+
+def eulerAnglesFromRxyz(Rxyz):
+    phi = np.arctan2(Rxyz[1][2], Rxyz [2][2])
+    theta = np.arcsin (Rxyz[0][2])
+    psi = np.arctan2(Rxyz[0][1], Rxyz[0][0])
+    return (phi, theta, psi)
+
+def eulerAnglesFromRzxz(Rzxz):
+    phi = np.arctan2(Rzxz[0][2], Rzxz [1][2])
+    theta = np.arccos (Rzxz[2][2])
+    psi = np.arctan2(Rzxz[2][0], -Rzxz[2][1])
+    return (phi, theta, psi)
+
 # Examples angles
 phi_deg = -30.0
 theta_deg = 65.0
@@ -42,11 +61,7 @@ psi = degToRad (psi_deg)
 Rxyz = rotationEulerXYZ (phi, theta, psi)
 print (f"Rxyz {Rxyz}")
 
-def eulerAnglesFromRxyz(Rxyz):
-    phi = np.arctan2(Rxyz[1][2], Rxyz [2][2])
-    theta = np.arcsin (Rxyz[0][2])
-    psi = np.arctan2(Rxyz[0][1], Rxyz[0][0])
-    return (phi, theta, psi)
+
 
 attitude = eulerAnglesFromRxyz(Rxyz)
 
@@ -55,3 +70,13 @@ theta2_deg = radToDeg(attitude[1])
 psi2_deg = radToDeg(attitude[2])
 
 print (f"Recovered Euler Angles[{phi2_deg} {theta2_deg} {psi2_deg}]")
+
+Rzxz = rotationEulerZXZ(phi, theta, psi)
+print (f"Rzyz [{Rzxz}]")
+
+attitudeZXZ =  eulerAnglesFromRzxz(Rzxz)
+phi2ZXZ_deg = radToDeg(attitudeZXZ[0])
+theta2ZXZ_deg = radToDeg(attitudeZXZ[1])
+psi2ZXZ_deg = radToDeg(attitudeZXZ[2])
+
+print (f"Recovered ZXZ Euler Angles[{phi2ZXZ_deg} {theta2ZXZ_deg} {psi2ZXZ_deg}]")
